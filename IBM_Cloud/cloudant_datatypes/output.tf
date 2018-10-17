05c680bff9ea80b8f33acc1c998074e5cf3e803e
Agostino Colussi <agostino.colussi@it.ibm.com>
2018-10-17 09:50:40 +0000

    Cloudant changes and node.js app

---
+++ IBM_Cloud/cloudant_datatypes/output.tf
@@ -0,0 +1,36 @@
+
+output "access_urls" {
+  value = "${module.cloudant_service.access_urls}"
+}
+
+output "host" {
+  value = "${module.cloudant_service.access_urls["host"]}"
+}
+output "port" {
+  value = "${module.cloudant_service.access_urls["port"]}"
+}
+output "password" {
+  value = "${module.cloudant_service.access_urls["password"]}"
+}
+output "url" {
+  value = "${module.cloudant_service.access_urls["url"]}"
+}
+output "username" {
+  value = "${module.cloudant_service.access_urls["username"]}"
+}
+output "apikey" {
+  value = "${module.cloudant_service.access_urls["apikey"]}"
+}
+output "iam_apikey_description" {
+  value = "${module.cloudant_service.access_urls["iam_apikey_description"]}"
+}
+output "iam_apikey_name" {
+  value = "${module.cloudant_service.access_urls["iam_apikey_name"]}"
+}
+output "iam_role_crn" {
+  value = "${module.cloudant_service.access_urls["iam_role_crn"]}"
+}
+output "iam_serviceid_crn" {
+  value = "${module.cloudant_service.access_urls["iam_serviceid_crn"]}"
+}
+
