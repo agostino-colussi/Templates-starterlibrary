fe29d49d9408a6115c154f9b3015036bb827f0cf
agostino colussi <agostino.colussi@it.ibm.com>
2019-04-11 14:54:28 +0000

    test json passing

---
+++ Agostino_Templates/Test_json_passing/README.md
@@ -0,0 +1,8 @@
+## nodejs_app infrastructure
+
+Created 10/17/18 by admin. for nodejs_app
+
+
+## Design Decisions
+| Design item                | Decision|
+| :----------------------------------- | :--------------------------------------------------------------------------------|
---
+++ Agostino_Templates/Test_json_passing/main.tf
@@ -0,0 +1,44 @@
+#####################################################################
+##
+##      Created 10/17/18 by admin. for nodejs_app
+##
+#####################################################################
+
+
+
+provider "null" {
+  version = "~> 0.1"
+}
+
+resource "null_resource" "app_vm" {
+  connection {
+    host = "${var.vm_address}"
+    type = "ssh"
+    user = "${var.ssh_user}"
+    password = "${var.ssh_user_password}"
+    }
+
+	provisioner "file" {
+    content = <<EOF
+#!/bin/bash
+#    
+#
+cd /tmp
+md test_json
+cd ./test.json
+touch vcap-local.json
+chown root:root vcap-local.json
+chmod 755 vcap-local.json
+row="${var.cloudant_cred}"
+echo "$row" >> vcap-local.json
+	EOF
+    destination = "/tmp/create_json.sh"
+  }
+  
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/create_json.sh; bash /tmp/create_json.sh",      
+    ]
+  } 
+  
+ } 
