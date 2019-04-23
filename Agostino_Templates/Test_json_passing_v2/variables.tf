4b0eda8a664db880e692338f6de4ea417505c2b9
agostino colussi <agostino.colussi@it.ibm.com>
2019-04-23 09:00:48 +0000

    fix1 ?

--- Agostino_Templates/Test_json_passing_v2/variables.tf
+++ Agostino_Templates/Test_json_passing_v2/variables.tf
@@ -18,7 +18,7 @@ variable "ssh_user_password" {
 }
 
 variable "cloudant_cred" {
-  value = "${base64encode(cloudant_cred)}"
+  value = "${base64encode(var.cloudant_cred)}"
   description = "cloudant credentials"
 }
 
