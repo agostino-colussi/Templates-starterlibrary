f7d558e48ec1e91da4f978bd54597639887966e3
agostino colussi <agostino.colussi@it.ibm.com>
2019-05-06 14:37:15 +0000

    fix

--- Agostino_Templates/cloudant_credentials_creation_v2/main.tf
+++ Agostino_Templates/cloudant_credentials_creation_v2/main.tf
@@ -26,8 +26,18 @@ ibmcloud login -a https://cloud.ibm.com -r us-south >> /tmp/create_cloudant_cred
 ibmcloud target --cf-api https://api.us-south.cf.cloud.ibm.com -o agostino.colussi -s Test >> /tmp/create_cloudant_credentials.log 2>&1
 ibmcloud service key-create ${var.service_name} ${var.service_credentials_name} >> /tmp/create_cloudant_credentials.log 2>&1
 com_response=$(ibmcloud service key-show ${var.service_name} ${var.service_credentials_name})
-com_output=$(echo $com_response | awk 'match($0,"{"){print substr($0,RSTART)}')
-echo $com_output
+
+apikey_var=$(echo $com_response | awk 'match($0,"{"){print substr($0,RSTART)}' | jq '.apikey')
+host_var=$(echo $com_response | awk 'match($0,"{"){print substr($0,RSTART)}' | jq '.host')
+iam_apikey_description_var=$(echo $com_response | awk 'match($0,"{"){print substr($0,RSTART)}' | jq '.iam_apikey_description')
+iam_apikey_name_var=$(echo $com_response | awk 'match($0,"{"){print substr($0,RSTART)}' | jq '.iam_apikey_name')
+iam_serviceid_crn_var=$(echo $com_response | awk 'match($0,"{"){print substr($0,RSTART)}' | jq '.iam_serviceid_crn')
+password_var=$(echo $com_response | awk 'match($0,"{"){print substr($0,RSTART)}' | jq '.password')
+port_var=$(echo $com_response | awk 'match($0,"{"){print substr($0,RSTART)}' | jq '.port')
+url_var=$(echo $com_response | awk 'match($0,"{"){print substr($0,RSTART)}' | jq '.url')
+username_var=$(echo $com_response | awk 'match($0,"{"){print substr($0,RSTART)}' | jq '.username')
+
+return_result="{\"apikey\": ${apikey_var}, \"host\": \"${host_var}\", \"iam_apikey_description\": \"${iam_apikey_description_var}\", \"iam_apikey_name\": \"${iam_apikey_name_var}\", \"iam_serviceid_crn\": \"${iam_serviceid_crn_var}\", \"password\": \"${password_var}\", \"port\": \"${port_var}\", \"url\": \"${url_var}\", \"username\": \"${username_var}\"}"
 EOF
     destination = "/tmp/create_credentials.sh"
   }
--- Agostino_Templates/cloudant_credentials_creation_v2/outputs.tf
+++ Agostino_Templates/cloudant_credentials_creation_v2/outputs.tf
@@ -4,11 +4,43 @@
 ##
 #####################################################################
 
-output "remote_script_stdout" {
+output "cloudant_credentials" {
   value = "${camc_scriptpackage.CreateScript.result}"
 }
 
 output "apikey" {
   value = "${lookup(camc_scriptpackage.CreateScript.result, "apikey")}"
 }
+  
+output "host" {
+  value = "${lookup(camc_scriptpackage.CreateScript.result, "host")}"
+}
+  
+output "iam_apikey_description" {
+  value = "${lookup(camc_scriptpackage.CreateScript.result, "iam_apikey_description")}"
+}
+  
+output "iam_apikey_name" {
+  value = "${lookup(camc_scriptpackage.CreateScript.result, "iam_apikey_name")}"
+}
+  
+output "iam_serviceid_crn" {
+  value = "${lookup(camc_scriptpackage.CreateScript.result, "iam_serviceid_crn")}"
+}
+  
+output "password" {
+  value = "${lookup(camc_scriptpackage.CreateScript.result, "password")}"
+}
+  
+output "port" {
+  value = "${lookup(camc_scriptpackage.CreateScript.result, "port")}"
+}
+  
+output "url" {
+  value = "${lookup(camc_scriptpackage.CreateScript.result, "url")}"
+}
+  
+output "username" {
+  value = "${lookup(camc_scriptpackage.CreateScript.result, "username")}"
+} 
 
