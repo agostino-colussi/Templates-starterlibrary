30c03cfeb48b501020341b78d4c79e191c55425c
agostino colussi <agostino.colussi@it.ibm.com>
2019-05-13 14:04:53 +0000

    Many changes

--- Agostino_Templates/cloudant/output.tf
+++ Agostino_Templates/cloudant/output.tf
@@ -2,11 +2,9 @@
 output "access_urls" {
   value = "${module.cloudant_service.access_urls}"
 }
-
 output "service_instance_name" {
   value = "${module.cloudant_service.ibm_service_instance.service.name}"
 }
-
 output "host" {
   value = "${module.cloudant_service.access_urls["host"]}"
 }
@@ -22,19 +20,3 @@ output "url" {
 output "username" {
   value = "${module.cloudant_service.access_urls["username"]}"
 }
-output "apikey" {
-  value = "${module.cloudant_service.access_urls["apikey"]}"
-}
-output "iam_apikey_description" {
-  value = "${module.cloudant_service.access_urls["iam_apikey_description"]}"
-}
-output "iam_apikey_name" {
-  value = "${module.cloudant_service.access_urls["iam_apikey_name"]}"
-}
-output "iam_role_crn" {
-  value = "${module.cloudant_service.access_urls["iam_role_crn"]}"
-}
-output "iam_serviceid_crn" {
-  value = "${module.cloudant_service.access_urls["iam_serviceid_crn"]}"
-}
-
--- Agostino_Templates/cloudant_credentials_creation/README.md
+++
@@ -1,8 +0,0 @@
-## Cloudant credentials creation
-
-Created on 4/9/19 by Agostino for Cloudant credentials creation
-
-
-## Design Decisions
-| Design item                | Decision|
-| :----------------------------------- | :--------------------------------------------------------------------------------|
